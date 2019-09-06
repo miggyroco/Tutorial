2c3211185a9492148bad92e4dc2ddfe72af463b5
Miggy Roco <miggyroco@gmail.com>
2019-09-06 17:07:48 +0800

    First Commit

---
+++ Training18 - Encapsulation and the API Docs/src/AppEncapsulation.java
@@ -0,0 +1,42 @@
+
+
+class Plant{
+		
+	public static final int ID = 7;
+	private String name;
+
+	
+	
+	public String getData() {
+		String data = "some stuff" + calculateGrowthForecast();
+		return data;
+	}
+	
+	private int calculateGrowthForecast() {
+		
+		return 9;
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
+
+
+public class AppEncapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
