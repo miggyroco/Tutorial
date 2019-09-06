41fea2994dc191d1be450747dd07230abd178fca
miggy <miggyroco@gmail.com>
2019-09-06 10:18:16 +0800

    First Commit
    Uploadin all files

---
+++ Training14Packages/src/AppPackages.java
@@ -0,0 +1,23 @@
+
+//import Fish in package folder ocean
+//import ocean.Fish;
+//import ocean.Seaweed;
+
+
+// *  all of the class in the packages
+import ocean.*;
+
+import ocean.plants.Algae;
+
+public class AppPackages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Fish fish = new Fish();
+		Seaweed weed = new Seaweed();
+		Algae algae = new Algae();
+
+	}
+
+}
